f8924865b8d94e794f633a63b34af6fc13c5efc6
Yannick Pfeifer <yannick-dev@tutanota.com>
2021-04-20 08:58:21 +0200

    ✨ feat(main): added graphiql

--- backend/src/deps.ts
+++ backend/src/deps.ts
@@ -1,2 +1,3 @@
 export { Application, Router } from "https://deno.land/x/oak@v6.5.0/mod.ts";
-export type { RouterMiddleware } from "https://deno.land/x/oak@v6.5.0/mod.ts";
+export type { RouterMiddleware, RouterContext } from "https://deno.land/x/oak@v6.5.0/mod.ts";
+export { applyGraphQL, gql, GQLError } from "https://deno.land/x/oak_graphql@0.6.2/mod.ts";
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,63 @@
-import { Application } from "./deps.ts";
-import router from "./routes/index.routes.ts";
+import { Application, Router, RouterContext } from "./deps.ts";
+import { applyGraphQL, gql, GQLError } from "./deps.ts";
 
 const app = new Application();
-app.use(router.routes());
-app.use(router.allowedMethods());
 
+const types = gql`
+type User {
+  firstName: String
+  lastName: String
+}
+
+input UserInput {
+  firstName: String
+  lastName: String
+}
+
+type ResolveType {
+  done: Boolean
+}
+
+type Query {
+  getUser(id: String): User 
+}
+
+type Mutation {
+  setUser(input: UserInput!): ResolveType!
+}
+`;
+
+const resolvers = {
+  Query: {
+    getUser: (parent: any, { id }: any, context: any, info: any) => {
+      console.log("id", id, context);
+      return {
+        firstName: "First",
+        lastName: "Last",
+      };
+    },
+  },
+  Mutation: {
+    setUser: (parent: any, { input: { firstName, lastName } }: any, context: any, info: any) => {
+      console.log("input:", firstName, lastName);
+      return {
+        done: true,
+      };
+    },
+  },
+};
+
+const GraphQLService = await applyGraphQL<Router>({
+  Router,
+  typeDefs: types,
+  resolvers: resolvers,
+  context: (ctx: RouterContext) => {
+    return { user: "Aaron" };
+  }
+})
+
+
+app.use(GraphQLService.routes(), GraphQLService.allowedMethods());
+
+console.log("Server start at http://localhost:8000");
 await app.listen({ port: 8000 });
