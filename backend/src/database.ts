93f58b4f142d4b283c357077ddbdd91aa1bafa04
David Tomschitz <david@tomschitz.de>
2021-04-30 12:50:48 +0200

    feat: added the database connection service

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,31 +1,24 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.22.0/mod.ts";
 
 export class DatabaseConnection {
-  public client: MongoClient;
+  client: MongoClient;
 
-  constructor(public dbName: string, public url: string) {
+  constructor(public name: string, public url: string) {
     this.client = {} as MongoClient;
   }
 
-  public connect() {
+  public async connect() {
     const client = new MongoClient();
-    client.connect(this.url);
+    await client.connect(this.url);
     this.client = client;
   }
 
   public getDatabase() {
-    try {
-      return this.client.database(this.dbName);
-    } catch (e) {
-      console.log(e);
-    }
+    return this.client.database(this.name);
   }
 }
 
-const database = new DatabaseConnection(
-  "saturn",
-  "mongodb://root:rootPassword@localhost:27017"
-);
-database.connect();
+const database = new DatabaseConnection("saturn", "mongodb://localhost:27017");
+await database.connect();
 
 export { database };
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,8 @@
-import { gqlTypes } from './schema/gql.types.ts';
+import { gqlTypes } from "./schema/gql.types.ts";
 import { Application, Router, RouterContext } from "./deps.ts";
 import { applyGraphQL, GQLError } from "./deps.ts";
 import { resolvers } from "./resolver/resolver.ts";
+import { database } from "./database.ts";
 
 const app = new Application();
 
@@ -11,11 +12,12 @@ const GraphQLService = await applyGraphQL<Router>({
   resolvers: resolvers,
   context: (ctx: RouterContext) => {
     return ctx;
-  }
-})
-
+  },
+});
 
 app.use(GraphQLService.routes(), GraphQLService.allowedMethods());
+
+const author = database.getDatabase()?.collection("game");
 
 console.log("Server start at http://localhost:8000");
 await app.listen({ port: 8000 });
