cdee933dba250f7ea5bad633c06465aa11eb9eaf
Bekemeier Fabian <fb084@hdm-stuttgart.de>
2021-05-01 19:26:20 +0200

    Revert "Merge branch 'feature/login' into 'master'"
    This reverts merge request !2

--- backend/src/deps.ts
+++
@@ -1,3 +0,0 @@
-export { Application, Router } from "https://deno.land/x/oak@v6.5.0/mod.ts";
-export type { RouterMiddleware, RouterContext } from "https://deno.land/x/oak@v6.5.0/mod.ts";
-export { applyGraphQL, gql, GQLError } from "https://deno.land/x/oak_graphql@0.6.2/mod.ts";
--- backend/src/routes/index.routes.ts
+++
@@ -1,9 +0,0 @@
-import { NestableRouter } from "./NestableRouter.ts";
-
-const router = new NestableRouter();
-
-router.get("/", (ctx:any) => {
-  ctx.response.body = "Hello World!";
-});
-
-export default router.normalize();
