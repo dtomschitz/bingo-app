849291bae80593f475019dfdc6f9692fd9295f5c
David Tomschitz <david@tomschitz.de>
2021-05-19 16:35:10 +0200

    feat: added the testWithClient function for testing towards a mongo db

---
+++ apps/backend/tests/test.deps.ts
@@ -0,0 +1,7 @@
+export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.22.0/mod.ts";
+export {
+  assert,
+  assertEquals,
+  assertThrows,
+  assertThrowsAsync,
+} from "https://deno.land/std@0.96.0/testing/asserts.ts";
