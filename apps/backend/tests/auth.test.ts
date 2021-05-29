1cb6f79e6595debe8b34aed05eb4a21977d0fc00
Staudenmaier Maximilian <ms497@hdm-stuttgart.de>
2021-05-29 13:22:32 +0200

    added loginUser tests

--- apps/backend/tests/auth.test.ts
+++ apps/backend/tests/auth.test.ts
@@ -10,7 +10,7 @@ import {
 } from "./test.deps.ts";
 import { AuthController } from "../src/controller/index.ts";
 import { Database, UserDatabase } from "../src/database/index.ts";
-import { ErrorType, RegisterProps } from "../src/models.ts";
+import { ErrorType, RegisterProps, LoginProps } from "../src/models.ts";
 
 import "https://deno.land/x/dotenv/load.ts";
 
@@ -22,6 +22,9 @@ describe("Authentication", () => {
   beforeAll(async () => {    
     const databaseUser = Deno.env.get("MONGO_ROOT_USER");
     const databasePassword = Deno.env.get("MONGO_ROOT_PASSWORD");    
+
+    console.log(databaseUser);
+    console.log(databasePassword);
 
     database = new Database(
       "saturn_testing",
@@ -118,4 +121,63 @@ describe("Authentication", () => {
       assertEquals(result.user.name, props.name);
     });
   })
+
+
+  describe("loginUser", () => {
+    it("should fail because login request is incorrect", async () => {
+      const props: LoginProps = {
+        email: "",
+        password: "",
+      };
+  
+      await assertThrowsAsync(
+        async () => await controller.loginUser(props),
+        GQLError,
+        ErrorType.INCORRECT_REQUEST,
+      );
+    });
+
+    it("should fail because the given login password is invalid", async () => {
+      const props: LoginProps = {
+        email: "test@test.de",
+        password: "",
+      };
+  
+      await assertThrowsAsync(
+        async () => await controller.loginUser(props),
+        GQLError,
+        ErrorType.INVALID_PASSWORD_FORMAT,
+      );
+    });
+  
+    it("should fail because the given email is invalid", async () => {
+      const props: LoginProps = {
+        email: "test@test.",
+        password: "SuperSicheresPasswort#1337#%",
+      };
+  
+      await assertThrowsAsync(
+        async () => await controller.loginUser(props),
+        GQLError,
+        ErrorType.INVALID_EMAIL_FORMAT,
+      );
+    });
+  
+    it("should validate the user and return the jwt tokens", async () => {
+      const props: LoginProps = {
+        email: "test@hallo.de",
+        password: "SuperSicheresPasswort#1337#%",
+      };
+  
+      const result = await controller.loginUser(props);
+  
+      assertExists(result.user._id)
+      assertExists(result.accessToken)
+      assertExists(result.refreshToken)
+
+      assertEquals(result.user.email, props.email);
+    });
+  })
+
+
 });
