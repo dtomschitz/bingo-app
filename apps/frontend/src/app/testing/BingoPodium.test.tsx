1a68467561e0aa802ed55d198d08a7fe309a061e
Fabian <bekemeier.fabian@gmail.com>
2021-06-22 20:01:34 +0200

    Add Test for BingoPodium and added --watch command for frontend tests

---
+++ apps/frontend/src/app/testing/BingoPodium.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import BingoPodium from '../components/bingo/BingoPodium';
+import { Podium } from '@bingo/models';
+
+it('renders BingoPodium', () => {
+  const podium: Podium[] = [
+    {
+      userId: '123',
+      name: 'third place',
+      placement: 3,
+    },
+    {
+      userId: '123',
+      name: 'second place',
+      placement: 2,
+    },
+    {
+      userId: '123',
+      name: 'first place',
+      placement: 1,
+    },
+  ];
+
+  const component = render(<BingoPodium podium={podium} />);
+
+  expect(component).toBeTruthy();
+
+  const winners = component.baseElement.textContent;
+
+  expect(winners).toContain('🥇 first place');
+  expect(winners).toContain('🥈 second place');
+  expect(winners).toContain('🥉 third place');
+});
