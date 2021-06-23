3f6705535220bb3cfdda98b380ce7cfbe6ef3b22
Yannick Pfeifer <yannick-dev@tutanota.com>
2021-06-23 19:41:40 +0200

    🐛 fix(tooling): backend test path

---
+++ apps/backend/tests/index.ts
@@ -0,0 +1,2 @@
+import "./auth.test.ts";
+import "./game.test.ts";
--- tools/test.sh
+++ tools/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -z "${NODE_VERSION}" ]]; then
-  /wait && deno test ./apps/backend/tests/auth.test.ts --allow-net --allow-env --allow-read --unstable
+  /wait && deno test ./apps/backend/tests/index.ts --allow-net --allow-env --allow-read --unstable
 else
   npm run frontend:test
 fi
